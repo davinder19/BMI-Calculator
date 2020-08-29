4853ff97d4941c137e03edc6b2111d744f5c1850
davinder19 <48900931+davinder19@users.noreply.github.com>
2020-08-29 04:23:19 -0700

    Add files via upload

--- bmi.js
+++ bmi.js
@@ -1,106 +1,113 @@
+let gander= document.getElementById('gander');
 let height = document.getElementById('height');
 let inch = document.getElementById('inch');
 let input = document.getElementById('input');
 let bm = document.getElementById('bm');
 let protien = document.getElementById('protien');
 let show = document.getElementById('show');
-let gander= document.getElementById('gander');
 let message = document.getElementById('message');
-let cal = document.getElementById('cal').addEventListener("click", gancheck);
+let cal = document.getElementById('cal').addEventListener("click", gencheck);
+let test , h, inc, bmi, pro, total, m, weight, pound;
+d =10;
+t=0;
+// p=0;
+cont=0;
 show.style.display='none';
 message.style.display='none';
-let bmi = 0;
-let h = 0;
-let pro = 0;
-let inc = 0
-let total =0;
-// console.log(total)
-let m= 0;
-// console.log(m);
-let weight =0;
-// console.log(weight);
-let pound = Math.round(weight* 2.20462262);
-// console.log(pound);
-function gancheck(e){
+gander.addEventListener('change', ()=>{
+    test = gander.options[gander.selectedIndex].value;
+    console.log(`i'm ${test}`);
+    });
+function gencheck (){
+    h =parseInt(height.value);
+    inc =parseInt(inch.value);
 
-  if(gander.value.toLowerCase()==="male"){
-    if(((height.value)> 0)&&((inch.value)>=0)){
-      h= parseInt(height.value); // assigne height value to h
-      // console.log(`height is feet ${h}`);
-      inc= parseInt(inch.value); // assign inch value to inc
-      // console.log(`height is inch ${inc}`);
-      weight = h* 10+(inc*2.3); // calculate weight for male
-      // console.log(`weight is ${weight}`);
-      calculate();
+
+    if (test==="male") {
+             if (h>0 && inc>0){
+                 console.log(`i'm ${test}`);
+                 console.log(`i'm ${h}`);
+           
+                 
+                 weight = h* 10+(inc*2.3); // calculate weight for male
+                // weight=(cont+h)*(t-60);
+              
+                  console.log(`weight is ${weight}`);
+                  calculate();
+   
+               }
+           else{
+            console.log("Try agian");
+            warning();
+            clear();
+           }
     }
+    else if (test==="female"){
+                     if (h>0 && inc>0){
+                       console.log(`i'm ${test}`);
+                       console.log(`i'm ${h}`);
+                       weight = h* 9.1+(inc*2.3);
+                       calculate();
+        
+                     }
+                     else{
+                        console.log("Try agian");
+                        warning();
+                         clear();
+                     }
+                    }
     else{
-      warning();
-      clear();
-    }
-    
-   }
-   else if(gander.value.toLowerCase()==="female"){
-    if(((height.value)> 0)&&((inch.value)>=0)){
-      h= parseInt(height.value); // assigne height value to h
-      // console.log(`height is feet ${h}`);
-      inc= parseInt(inch.value); // assign inch value to inc
-      // console.log(`height is inch ${inc}`);
-      weight = h* 9.1+(inc*2.3); // calculate weight for female
-      // console.log(`weight is ${weight}`);
-      calculate();
-      }
-      else{
+        console.log("TRY AGAIN");
         warning();
         clear();
-      }   }
-   else{
-     console.log("Please Enter Correct info");
-     warning();
-     clear();
-   }
-   e.preventDefault();
+        }
+    }
 
-}
-function calculate(){
-     message.style.display='none'; //display message none
-    show.style.display= '' // disply show id box if result show
-    //  console.log(`total is ${total}`);
-     // BMI calculate
-    //  console.log("I'm working");
-     total = h + '.' + inc;
-     m = (total * 0.3048).toFixed(1); //  feet convert to meter
-    //  console.log(`M is ${total}`);
-    //  console.log(`total is ${m}`);
-     bmi = weight/( m * m);
-     bmi =  (bmi);
-    //  console.log(`youor BMI is ${bmi}`);
-     bm.value= Math.round(bmi);
-     //protien need every day
-     //0.8g * weught in kg
-     pro = Math.round(0.8 * weight);
-     protien.value = pro;
-    //  console.log(`Yourprotien need is ${pro}`);
-    //weight kg convert to pound
-      weight = Math.round(weight* 2.20462262);
-    //  console.log(`Your good weight is ${weight}`);
-     input.value = weight;
-     // clear input
-      clear();
-    setTimeout(clearResult, 8000);
-}
-function warning(){
+                // calculate function
+                function calculate(){
+                    message.style.display='none'; //display message none
+                   show.style.display= '' // disply show id box if result show
+                   //  console.log(`total is ${total}`);
+                    // BMI calculate
+                   //  console.log("I'm working");
+                    total = h + '.' + inc;
+                    m = (total * 0.3048).toFixed(1); //  feet convert to meter
+                   //  console.log(`M is ${total}`);
+                   //  console.log(`total is ${m}`);
+                    bmi = weight/( m * m);
+                    bmi =  (bmi);
+                   //  console.log(`youor BMI is ${bmi}`);
+                    bm.value= Math.round(bmi);
+                    //protien need every day
+                    //0.8g * weught in kg
+                    pro = Math.round(0.8 * weight);
+                    protien.value = pro;
+                   //  console.log(`Yourprotien need is ${pro}`);
+                   //weight kg convert to pound
+                     weight = Math.round(weight* 2.20462262);
+                   //  console.log(`Your good weight is ${weight}`);
+                    input.value = weight;
+                    // clear input
+                     clear();
+                   setTimeout(clearResult, 8000);
+               }
+               //warning function
+               function warning(){
 
-message.style.display ='';
-show.style.display='none';
-setTimeout(clearResult, 3000);
-}
-function clear(){
-   height.value= '';
-   inch.value = '';
-   gander.value='';
-}
-function clearResult(){
-
-  show.style.display='none';
-  message.style.display ='none';
-}
+                message.style.display ='';
+                show.style.display='none';
+                setTimeout(clearResult, 3000);
+                }
+                //clear function
+                function clear(){
+                   height.value= '';
+                   inch.value = '';
+    
+                }
+                // clearResult function
+                function clearResult(){
+                
+                  show.style.display='none';
+                  message.style.display ='none';
+                }
+                
